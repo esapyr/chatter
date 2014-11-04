1aeefce495261a87a2d16e932d26c8a0205bcde0
Ryan Serazin <rps9530@rit.edu>
2014-11-04 15:26:03 -0500

    Generally finished aside from rooting out bugs. May be misunderstanding the manifold streams returned by the websocket util functions.

--- project.clj
+++ project.clj
@@ -5,10 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [aleph "0.4.0-alpha7"]
-                 [compojure "1.2.1"]
-                 [ring "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [aleph "0.4.0-alpha7"]]
   :main ^:skip-aot chatter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chatter/core.clj
+++ src/chatter/core.clj
@@ -1,26 +1,61 @@
 (ns chatter.core
   (:gen-class)
-  (:require [compojure.core :refer [defroutes GET POST]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [clojure.core.async :refer [go chan thread go-loop alts! close! >! <! >!! <!!]]
+  (:require [clojure.core.async :refer [chan alts! <! >!]]
+            [manifold.deferred :as d]
             [manifold.stream :as s]
             [aleph.http :as http]))
 
 (def clients (atom #{}))
-(def TIMEOUT 30000)
 
-(defn echo-handler [req]
-  (let [s @(http/websocket-connection req)]
-    (s/connect s s)))
+(defn server-handle-client [req]
+  (println "~Using req to connect~")
+  (let [deferred-strm (http/websocket-connection req)]
+    (d/on-realized deferred-strm                                      ;once we have the connection
+                   (fn [strm]                                         ;strm = this client's stream
+                     (println "!Connected!")
+                     (swap! clients conj strm)                        ;put connection in set
+                     (s/consume (fn [msg]                             ;consume every message from a client, and send to all other clients
+                                  (doseq [c-strm @clients]            ;for each client stream
+                                    (when (not= c-strm strm)          ;if it's not the active client's stream
+                                      (println "sent " msg " to " c-strm)
+                                      (s/put! c-strm msg))))
+                                strm))                                ;client stream is closed over
+                   (fn [error] (println "error connecting to client: " error)))))
+
+(defn- validate-input[input]
+  (if (or (nil? input) (= ":close" input))
+    nil
+    input))
+
+(defn start-client []
+  (let [deferred-strm (http/websocket-client "ws://localhost:9099")]
+    (println "~Waiting for connection~")
+    (d/on-realized deferred-strm                                      ;once we have a connection
+                   (fn [c-strm]
+                     (println "!Connected!")
+                     (s/consume (fn [msg]                             ;consume all msg from server
+                                  (println msg))                      ;may have to sync
+                                c-strm)
+                     (loop []
+                       (prn ">")
+                       (when-let [res (validate-input (read-line))]
+                         (s/put! c-strm res)
+                         (recur))))
+                   (fn [error] (println "error connecting to server: " error)))))
 
 (defn -main
   [& args]
   (case (first args)
-    "server" (let [port (or (second args) 9090)]
-               (prn "started server")
-               (http/start-server echo-handler {:port 9099})
-               )))
+    "client" (start-client)
+    "server" (let [port (or (second args) 9099)]
+               (println "~Starting server~")
+               (println "Waiting for connections...")
+               (http/start-server server-handle-client {:port (eval port)}))))
+
+#_(defn echo-handler [req]
+    (let [s @(http/websocket-connection req)]
+      (s/connect s s)))
 
 #_(let [c @(http/websocket-client "ws://localhost:9099")]
-  (s/put! c "wor")
-  (prn @(s/take! c)))
+    (s/put! c "wor")
+    (prn @(s/take! c)))
